2f119f55a0c7ec1db9aaa860ecb2e4e39baee005
Martin Keršner <m.kersner@gmail.com>
2015-12-18 14:48:46 +0900

    added window_cls_data_layer, successful build

--- include/caffe/layers/data_layers.hpp
+++ include/caffe/layers/data_layers.hpp
@@ -541,7 +541,7 @@ class WindowClsDataLayer : public ImageDimPrefetchingDataLayer<Dtype> {
       const vector<Blob<Dtype>*>& top);
 
   //virtual inline LayerParameter_LayerType type() const {
-  virtual inline V1LayerParameter_LayerType type() const { // Martin Kersner, 2015/12/16
+  virtual inline V1LayerParameter_LayerType V1type() const { // Martin Kersner, 2015/12/16
     //return LayerParameter_LayerType_IMAGE_DATA;
     return V1LayerParameter_LayerType_IMAGE_DATA; // Martin Kersner, 2015/12/16
   }
