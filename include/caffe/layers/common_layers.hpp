a5a8b23dc6bd2719f7578791e9ca9d801445b5f3
Martin Keršner <m.kersner@gmail.com>
2015-12-18 11:17:02 +0900

    SUCCESSFUL BUILD: added bn_layer

--- include/caffe/layers/common_layers.hpp
+++ include/caffe/layers/common_layers.hpp
@@ -103,7 +103,7 @@ class BNLayer : public Layer<Dtype> {
       const vector<Blob<Dtype>*>& top);
 
   //virtual inline LayerParameter_LayerType type() const {
-  virtual inline V1LayerParameter_LayerType type() const { // Martin Kersner, 2015/12/16
+  virtual inline V1LayerParameter_LayerType V1type() const { // Martin Kersner, 2015/12/16
     //return LayerParameter_LayerType_BN;
     return V1LayerParameter_LayerType_BN; // Martin Kersner, 2015/12/16
   }
